448b24a8ca7d5151c3c0a3571e699bba41bed0f1
SohamDasIsCool <72563026+SohamDasIsCool@users.noreply.github.com>
2021-05-27 15:16:34 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ function jump(){
         dino.classList.remove("jump")
     },500);
 }
+function bom(){
+x="true";
+document.getElementById("boop").innerHTML="SUPER JUMP ACTIVATED";
+}
 function glide(){
     if(x=="false"){
     }
@@ -18,6 +22,10 @@ function glide(){
     },1500);
 }
 }
+setInterval(function(){
+    cool= /\d+/g;
+    y=document.getElementById("scoreSpan").innerHTML ="Your score:"+Math.floor(counter/100);
+},1000);
 var isAlive=setInterval(function() {
     dinoTop=parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
     cactusleft=parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
@@ -27,11 +35,6 @@ var isAlive=setInterval(function() {
         counter=0;
         cactus.style.animation = "swipe infinite linear 1s";
     }else{
-        counter++;
-        document.getElementById("scoreSpan").innerHTML ="Your score:"+Math.floor(counter/100);
-    }
-    if(counter=1000 || counter> 1000){
-        x="true";
         counter++;
         document.getElementById("scoreSpan").innerHTML ="Your score:"+Math.floor(counter/100);
     }
