18279b1ad650347ca1d68d83c16a114e3e5a59a0
SohamDasIsCool <72563026+SohamDasIsCool@users.noreply.github.com>
2021-06-02 17:58:50 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 dino=document.getElementById("dino");
 cactus=document.getElementById("cactus");
 counter=0;
+sound="";
+function preload(){
+    sound=loadSound("sound.mp3");
+}
 x="false";
 function jump(){
     dino.classList.add("jump");
+    document.getElementById("dino").src = "dino.PNG";
     setTimeout(function(){
-        dino.classList.remove("jump")
+        dino.classList.remove("jump");
     },500);
+    document.getElementById("dino").src = "Webp.net-gifmaker.gif";
 }
 function bom(){
 x="true";
 document.getElementById("boop").innerHTML="SUPER JUMP ACTIVATED";
+sound.play();
 }
 function glide(){
     if(x=="false"){
@@ -22,33 +29,41 @@ function glide(){
     },1500);
 }
 }
-setInterval(function(){
-    cool= /\d+/g;
-    y=document.getElementById("scoreSpan").innerHTML ="Your score:"+Math.floor(counter/100);
-},1000);
 var isAlive=setInterval(function() {
     dinoTop=parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
     cactusleft=parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
-    if(cactusleft<50 && cactusleft>0 && dinoTop>=150){
+    if(cactusleft<40 && cactusleft>0 && dinoTop>=140){
         cactus.style.animation = "none";
+        document.getElementById("dino").src = "dino.PNG";
         alert("Game Over. score: "+Math.floor(counter/100));
         counter=0;
         cactus.style.animation = "swipe infinite linear 1s";
     }else{
         counter++;
         document.getElementById("scoreSpan").innerHTML ="Your score:"+Math.floor(counter/100);
-        r=Math.floor(Math.random()*3);
-        if(r==0{
+    }
+},10);
+setInterval(
+    function(){
+        x=Math.floor(Math.random()*2);
+if(x==0){
+  document.getElementById("cactus").src = "cactus.png";
+  }
+  else{
+  document.getElementById("cactus").src = "tr.png";
+  }
+  r=Math.floor(Math.random()*3);
+        if(r==0){
         cactus.style.animation ="swipe infinite linear 1s";
            }
         if(r==1){
-            cactus.style.animation ="swipe infinite linear 0.9s";
+            cactus.style.animation ="swipe infinite linear 0.95s";
         }
         else{
-            cactus.style.animation ="swipe infinite linear 1.1s";
+            cactus.style.animation ="swipe infinite linear 1.05s";
         }
-    }
-},10);
+    },1000
+);
 document.body.onkeydown = function(e){
     if(e.keyCode == 32){
         jump();
